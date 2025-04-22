c931e83a50caf262b6aaecc7739872a930895d06
Matthijs <matthijs1000@gmail.com>
2025-04-22 12:58:08 +0200

    Ingen secrets

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, onValue, set } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC4J4rgn-L3iYaQgEU0o7Qj0Kw15VYG5zk",
-  authDomain: "avgrunnen-85b84.firebaseapp.com",
-  databaseURL: "https://avgrunnen-85b84-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "avgrunnen-85b84",
-  storageBucket: "avgrunnen-85b84.firebasestorage.app",
-  messagingSenderId: "1000199596801",
-  appId: "1:1000199596801:web:ecaec1ae47f7e14fad0900",
-  measurementId: "G-Q11EKSJGR2"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
